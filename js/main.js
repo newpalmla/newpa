d8a227c4ddf0734d7953a7e0b051c80fe161e9cd
Claire Kuang <kuang.claire@gmail.com>
2023-07-13 23:24:06 +0100

    fixes multiple event listener bug

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@
   // collapsible section events
   var coll = document.getElementsByClassName("collapsible");
   var i;
-
   for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
 
@@ -20,48 +19,58 @@
       else
       {
         for(item of coll){ 
-          if (item.classList)
-          item.classList.remove("active");
-          item.nextElementSibling.style.maxHeight = null;
+          if (item.classList.contains("active"))
+          {
+            item.classList.remove("active");
+            item.nextElementSibling.style.maxHeight = null;
+          }
         }
         content.style.maxHeight = content.scrollHeight + "px";
-
-        // get the collapsible items in this collapsible section
         var parentHeight = content.style.maxHeight;
-        var collItems = content.getElementsByClassName("collapsible-item");
-        var j;
-        for (j = 0; j < collItems.length; j++) {
-          collItems[j].addEventListener("click", function() {
-            
-            var itemContent = this.nextElementSibling;
-            if (this.classList.contains("active-item"))
-            {
-              content.style.maxHeight -= itemContent.scrollHeight;
-              itemContent.style.maxHeight = null;
-              this.classList.remove("active-item");
-              itemContent.classList.remove("has-content") 
-            }
-            else
-            {
-              for(item of collItems){ 
-                if (item.classList)
-                item.classList.remove("active-item");
-                item.nextElementSibling.style.maxHeight = null;
-                item.nextElementSibling.classList.remove("has-content") 
-              }
-              itemContent.style.maxHeight = itemContent.scrollHeight + "px";
-              content.style.maxHeight = itemContent.scrollHeight + parentHeight;
-              this.classList.add("active-item"); 
-              itemContent.classList.add("has-content") 
-            }
 
-          });
+        // collapsible item events
+  var collItems = content.getElementsByClassName("collapsible-item");
+  var j;
+  for (j = 0; j < collItems.length; j++) {
+    var currentItem = collItems[j];
+    if (!currentItem.hasAttribute('listenerOnClick')) {
+      currentItem.addEventListener("click", function() {
+      
+        var itemContent = this.nextElementSibling;
+        if (this.classList.contains("active-item"))
+        {
+          content.style.maxHeight -= itemContent.scrollHeight;
+          itemContent.style.maxHeight = null;
+          this.classList.remove("active-item");
+          itemContent.classList.remove("has-content") 
         }
+        else
+        {
+          for(item of collItems){ 
+            if (item.classList.contains("active-item")) {
+              item.classList.remove("active-item");
+              item.nextElementSibling.style.maxHeight = null;
+              item.nextElementSibling.classList.remove("has-content"); 
+            }
+          }
+          itemContent.style.maxHeight = itemContent.scrollHeight + "px";
+          content.style.maxHeight = itemContent.scrollHeight + parentHeight;
+          this.classList.add("active-item"); 
+          itemContent.classList.add("has-content") 
+        }
+
+      });
+    }
+    currentItem.setAttribute('listenerOnClick', 'true');
+  }
+        
       }
       this.classList.toggle("active"); // toggle the active state of this collapsible
 
     });
   }
+
+  
 
   
   var isMobile = function () {
